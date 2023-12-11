eade70342fcb150444ccd1228a0d0e8e08d55bde
Rachel Galdino <rachel.galdino@outlook.com>
2023-12-11 19:24:44 -0300

    Add Hero class with attack method

---
+++ script.js
@@ -0,0 +1,31 @@
+class Hero {
+  constructor(name, age, type) {
+    this.name = name;
+    this.age = age;
+    this.type = type;
+  }
+
+  attack() {
+    switch (this.type) {
+      case 'warrior':
+        this.attack = 'a sword';
+        break;
+      case 'monk':
+        this.attack = 'martial arts';
+        break;
+      case 'magician':
+        this.attack = 'magic';
+        break;
+      case 'ninja':
+        this.attack = 'shuriken';
+        break;
+      default:
+        this.attack = 'an unknown weapon';
+    }
+
+    console.log(`The ${this.type} attacked using ${this.attack}!`);
+  }
+}
+
+let hero1 = new Hero('Shena', 20, 'warrior');
+console.log(hero1.attack());
